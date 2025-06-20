ea4b0a36090786eeecbca1832692fc0d9b133f31
Hai Lam <sunmobile2611@gmail.com>
2025-06-20 20:04:27 +0700

    Add deploy setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
+const repoName = 'my-quiz-app';
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [react(), tsconfigPaths()],
-})
+});
